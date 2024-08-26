ab871c714d43ab2bf8122721528959b581213703
Andrei <An.S-1@yandex.ru>
2024-08-27 00:19:41 +0300

    controllers-films-users 5

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserNotFondException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/UserNotFondException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class UserNotFondException extends RuntimeException{
+public class UserNotFondException extends RuntimeException {
     public UserNotFondException() {
     }
 }
