46f964bb623185c018c77400fcd9374e00248f68
Andrei <An.S-1@yandex.ru>
2024-10-24 22:29:25 +0300

    controllers-films-users

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class User {
+    private Long id;
+    @Email(message = "электронная почта не может быть пустой и должна содержать символ @")
+    private String email;
+    @NotEmpty(message = "логин не может быть пустым и содержать пробелы")
+    private String login;
+    private String name;
+    @PastOrPresent(message = "дата рождения не может быть в будущем.")
+    private LocalDate birthday;
+}
